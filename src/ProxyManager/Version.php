e7ed7ba2f12efe8502ca88e3c4aafb3af77b9a95
Daniel Toader <daniel.toader@emag.ro>
2023-01-11 17:30:16 +0200

    Update to support php 8.1

--- src/ProxyManager/Version.php
+++ src/ProxyManager/Version.php
@@ -34,7 +34,7 @@ final class Version
      */
     public static function getVersion(): string
     {
-        return (string) InstalledVersions::getPrettyVersion('ocramius/proxy-manager')
-            . '@' . (string) InstalledVersions::getReference('ocramius/proxy-manager');
+        return (string) InstalledVersions::getPrettyVersion('danieltoader/proxy-manager')
+            . '@' . (string) InstalledVersions::getReference('danieltoader/proxy-manager');
     }
 }
